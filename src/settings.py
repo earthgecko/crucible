5a32976b49a527cd87d5dd859f631deb68ec4ffd
Gary Wilson <gary.wilson@of-networks.co.uk>
2016-01-05 12:59:50 +0000

    Working in the 100 range Added boundary detect_drop_off_cliff example working in the 100 range Updated detect_drop_off_cliff algorithm with tested tweaks and removed the < 100 filter Updated to pep8 compliant Added: examples/detect_drop_off_cliff/detect_drop_off_cliff.in.the.100.range.boundary.alert.png examples/detect_drop_off_cliff/detect_drop_off_cliff.in.the.100.range.hours.later.comparison.png Modified: examples/detect_drop_off_cliff/readme.md src/algorithms.py src/crucible.py src/settings.py utils/graphite-grab.py .gitignore

--- src/settings.py
+++ src/settings.py
@@ -1,14 +1,12 @@
 # These are the algorithms that the crucible will run. To add a new algorithm,
 # you must both define the algorithm in algorithms.py and add its name here.
-ALGORITHMS = [
-                'first_hour_average',
-                'mean_subtraction_cumulation',
-                'stddev_from_average',
-                'stddev_from_moving_average',
-                # 'least_squares',
-                'grubbs',
-                'histogram_bins',
-                'median_absolute_deviation',
-                'ks_test',
-                'detect_drop_off_cliff',
-             ]
+ALGORITHMS = ['first_hour_average',
+              'mean_subtraction_cumulation',
+              'stddev_from_average',
+              'stddev_from_moving_average',
+              # 'least_squares',
+              'grubbs',
+              'histogram_bins',
+              'median_absolute_deviation',
+              'ks_test',
+              'detect_drop_off_cliff']
